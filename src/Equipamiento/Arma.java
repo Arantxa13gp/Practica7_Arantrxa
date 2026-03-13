d69981780560171f7ddab7fca519da03174ac5df
Arantxa13gp <arantxa13gp@gmail.com>
2026-03-13 16:30:01 +0100

    Primer commit

--- src/Equipamiento/Arma.java
+++ src/Equipamiento/Arma.java
@@ -1,14 +1,43 @@
 package Equipamiento;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Arma extends Equipamiento {
 
-    public Arma(String nombre, ArrayList<Integer> estadisticas, String rareza, int valor) {
-        super(nombre, estadisticas, rareza, valor);
-    }
+    private String empuñadura;
+    private String tipo;
 
+    public Arma(String nombre, ArrayList<Integer> estadisticas, String rareza, int valor, String empuñadura, String tipo) {
+        super(nombre, estadisticas, rareza, valor);
+        setEmpuñadura(empuñadura);
+        setTipo(tipo);
+    }
     public Arma(Equipamiento copia) {
         super(copia);
+
+    }
+
+    public String getEmpuñadura() {
+        return empuñadura;
+    }
+
+    public void setEmpuñadura(String empuñadura) {
+        this.empuñadura = empuñadura;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        switch (tipo.toLowerCase()){
+            case "una mano":
+                break;
+            case "dos manos":
+                break;
+            default:
+                return;
+        }
     }
 }
